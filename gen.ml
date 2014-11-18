d4df3262964168af9a3331e4173bd9e235d0ada2
Thomas McDonald <tom@conceptcoding.co.uk>
2014-11-18 15:15:24 +0000

    add class gen_stmt

--- gen.ml
+++ gen.ml
@@ -1,9 +1,24 @@
+open Tree
+
 let put (s : string) : unit =
   print_string (s ^ "\n");
   ()
 
-let generate (prog : Tree.program) : unit =
+let gen_stmt (stmt : stmt) =
+  match stmt with
+    ClassDecl(e, xs) ->
+      put "! Class Description";
+      (* class code *)
+      ()
+
+let gen_stmts (xs : stmt list) =
+  List.map gen_stmt xs
+
+let generate (prog : program) : unit =
   put "MODULE Main 0 0";
   put "IMPORT Lib 0";
   put "ENDHDR";
+  match prog with
+    Prog(Block (xs)) ->
+      gen_stmts xs;
   ()
