9044ee7a1b5aa0feab76d93e68aabd5cccbf9fde
Thomas McDonald <tom@conceptcoding.co.uk>
2014-11-18 15:35:47 +0000

    add () [] to the printed ast

--- print.ml
+++ print.ml
@@ -8,23 +8,28 @@ let nest xs =
   let comb = (fun acc x -> if acc == "" then x else acc ^ "\n  " ^ x) in
   List.fold_left comb "" lines
 
+let wrap_simple (s : string) = "(" ^ s ^ ")"
+
+let wraplist (id : string) (xs : string list) : string =
+  wrap_simple (id ^ (String.concat "" (List.map nest xs)))
+
 let rec ppExpr e =
   match e.e_guts with
-    Number x -> "Number " ^ string_of_int x
-  | Const x -> "Const " ^ x
-  | Ident x -> "Ident " ^ x
-  | Binop (Plus, e, e') -> "Add" ^ nest (ppExpr e) ^ nest (ppExpr e')
-  | Call (o, m, xs) -> "Call" ^ nest (ppExpr o) ^ nest (ppExpr m) (* ^ nest (List.map ppExpr xs) *)
+    Number x -> wrap_simple ("Number " ^ string_of_int x)
+  | Const x -> wrap_simple ("Const " ^ x)
+  | Ident x -> wrap_simple ("Ident " ^ x)
+  | Binop (Plus, e, e') -> wraplist "Add" [ppExpr e; ppExpr e'] (* ^ nest (ppExpr e) ^ nest (ppExpr e') *)
+  | Call (o, m, xs) -> wraplist "Call" [ppExpr o; ppExpr m] (* ^ nest (List.map ppExpr xs) *)
   | Nil -> "nil"
 
 let rec ppStmt =
   function
-    ClassDecl (e, e', xs) -> "Class" ^ nest (ppExpr e) ^ nest (ppExpr e') ^ nest (ppStmts xs)
-  | MethodDecl (e, xs) -> "Method" ^ nest (ppExpr e) ^ nest (ppStmts xs)
-  | Assign (e, e') -> "Assignment" ^ nest (ppExpr e) ^ nest (ppExpr e')
-  | Declare(e) -> "Declaration " ^ nest (ppExpr e)
+    ClassDecl (e, e', xs) -> wraplist "Class" [ppExpr e; ppExpr e'; ppStmts xs]
+  | MethodDecl (e, xs) -> wraplist "Method" [ppExpr e; ppStmts xs]
+  | Assign (e, e') -> wraplist "Assignment" [ppExpr e; ppExpr e']
+  | Declare(e) -> wraplist "Declaration " [ppExpr e]
 
-and ppStmts xs = String.concat "\n" (List.map ppStmt xs)
+and ppStmts xs = "[" ^ String.concat ",\n" (List.map ppStmt xs) ^ "]"
 
 let ppBlock =
   function
