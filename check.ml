25bcf5b25e4f298245695e5e63136b864e2bc45a
Thomas McDonald <tom@conceptcoding.co.uk>
2014-12-08 12:03:23 +0000

    rename the check_* methods to populate_* as a better descriptor

--- check.ml
+++ check.ml
@@ -8,8 +8,8 @@ let error (str:string) =
 let ensure_unique env x =
   if def_exists env x then error (x ^ " is already defined")
 
-(* check_class checks that the class name is unique & the superclass exists *)
-let check_class (klass : stmt) (env : environment) : environment =
+(* populate_class checks that the class name is unique & the superclass exists *)
+let populate_class (klass : stmt) (env : environment) : environment =
   match klass with
     ClassDecl(Object(n), Void, _) ->
       ensure_unique env n;
@@ -19,7 +19,7 @@ let check_class (klass : stmt) (env : environment) : environment =
       if class_exists env s then (define_class env n s) else error "superclass not defined"
   | _ -> error "check_class_name must be called with a class"
 
-let check_variable (dec : stmt) class_name env : environment =
+let populate_variable (dec : stmt) class_name env : environment =
   let c = find_def env class_name in
   match dec with
     Declare(Object(t), e) ->
@@ -36,17 +36,17 @@ let check_variable (dec : stmt) class_name env : environment =
       error "declare called with a primitive - TODO"
   | _ -> error "check variable called with a non var"
 
-let check_variables (klass : stmt) (env : environment) : environment =
+let populate_variables (klass : stmt) (env : environment) : environment =
   let statement_filter = (fun x -> match x with Declare(_) -> true | _ -> false) in
   match klass with
     ClassDecl(Object(n), _, xs) ->
-      let var_accu = (fun env x -> check_variable x n env) in
+      let var_accu = (fun env x -> populate_variable x n env) in
       let statements = List.filter statement_filter xs in
       List.fold_left var_accu env statements
   | _ -> error "check_variables called against a non-class"
 
 
-let check_method (meth : stmt) class_name env : environment =
+let populate_method (meth : stmt) class_name env : environment =
   let c = find_def env class_name in
   match meth with
     MethodDecl(e, xs) ->
@@ -59,29 +59,33 @@ let check_method (meth : stmt) class_name env : environment =
   | _ -> error "check_method called with a non-method stmt"
   env
 
-let check_methods (klass : stmt) env : environment =
+let populate_methods (klass : stmt) env : environment =
   let method_filter = (fun x -> match x with MethodDecl(_) -> true | _ -> false) in
   match klass with
     ClassDecl(Object(n), _, xs) ->
-      let meth_accu = (fun env x -> check_method x n env) in
+      let meth_accu = (fun env x -> populate_method x n env) in
       let statements = List.filter method_filter xs in
       List.fold_left meth_accu env statements
     | _ -> error "check_methods called against a non-class"
 
 (*
-  check_classes extracts information about the classes in the program.
+  populate_class_info extracts information about the classes in the program 
+    & adds it to the enviornment
   This is done in multiple passes.
     * Fold through the list of classes, gathering a list of class names and verifying
       * no duplicate names
       * superclasses are already defined
-    * Generate list of instance variables & check their types
+    * Generate list of instance variables
+      * Check types exist
+      * Names are not duplicate
     * Generate list of methods
+      * Names are not duplicate
     TODO: look into fusing some of these methods together
 *)
-let check_classes (classes : stmt list) (env : environment) =
-  let class_accu = (fun env x -> check_class x env) in
-  let var_accu = (fun env x -> check_variables x env) in
-  let meth_accu = (fun env x -> check_methods x env) in
+let populate_class_info (classes : stmt list) (env : environment) =
+  let class_accu = (fun env x -> populate_class x env) in
+  let var_accu = (fun env x -> populate_variables x env) in
+  let meth_accu = (fun env x -> populate_methods x env) in
   let env' = List.fold_left class_accu env classes in
   let env'' = List.fold_left var_accu env' classes in
   List.fold_left meth_accu env'' classes
@@ -89,7 +93,7 @@ let check_classes (classes : stmt list) (env : environment) =
 let check_block (env : environment) (block : block) =
   match block with
       NoBlock -> raise (Failure "no program given")
-    | Block(xs) -> check_classes xs env
+    | Block(xs) -> populate_class_info xs env
 
 let annotate (program : program) : unit =
   let env = initial_env in
